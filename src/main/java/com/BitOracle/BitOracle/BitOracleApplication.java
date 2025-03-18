d3c4e707edc7a29dc843928557b0d170b03d16fc
jhj2075 <flg989@naver.com>
2025-03-18 16:02:51 +0900

    project setting

---
+++ src/main/java/com/BitOracle/BitOracle/BitOracleApplication.java
@@ -0,0 +1,13 @@
+package com.BitOracle.BitOracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitOracleApplication.class, args);
+	}
+
+}
