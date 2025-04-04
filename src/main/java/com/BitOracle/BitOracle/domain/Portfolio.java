a5bbdb54bd04f99d0a47e6979bcebfe3bbb7e01b
jinseo123 <kjk0385@naver.com>
2025-04-04 20:34:56 +0900

    도메인 수정

---
+++ src/main/java/com/BitOracle/BitOracle/domain/Portfolio.java
@@ -0,0 +1,29 @@
+package com.BitOracle.BitOracle.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long profile_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Coin> coinList = new ArrayList<>();
+
+}
