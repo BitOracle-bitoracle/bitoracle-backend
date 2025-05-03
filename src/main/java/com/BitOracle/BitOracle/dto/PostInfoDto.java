d7d0bc8133eadb5c929412569262b505bbeaaf28
jinseo123 <kjk0385@naver.com>
2025-05-03 21:22:47 +0900

    fix : 게시물 단건조회

--- src/main/java/com/BitOracle/BitOracle/dto/PostInfoDto.java
+++ src/main/java/com/BitOracle/BitOracle/dto/PostInfoDto.java
@@ -32,14 +32,21 @@ public class PostInfoDto {
 
         this.writer = new MemberDto(post.getUser());
 
+        List<Reply> replies = post.getReplyList();
+
+        //부모댓글리스트
+        List<Reply> parentReplies = replies.stream()
+                .filter(reply -> reply.getParent() == null)
+                .toList();
         //댓글 대댓글 그룹짖기 post.getCommentList는 댓글과 대댓글 모두 조회 섞여잇는상태
-        Map<Reply, List<Reply>> replyListMap = post.getReplyList().stream()
+        //Map<댓글, List<해당 댓글에 달린 대댓글>>의 형식
+        Map<Reply, List<Reply>> replyListMap = replies.stream()
                 .filter(reply -> reply.getParent() != null)//대댓글만 선별
                 .collect(Collectors.groupingBy(Reply::getParent));
 
         //댓글과 대댓글로 replyList생성
-        replyList = replyListMap.keySet().stream()//댓글가지고오기
-                .map(reply -> new ReplyInfoDto(reply,replyListMap.get(reply)))
+        replyList = parentReplies.stream()//댓글가지고오기
+                .map(reply -> new ReplyInfoDto(reply,replyListMap.getOrDefault(reply, List.of())))
                 .toList();
 
     }
