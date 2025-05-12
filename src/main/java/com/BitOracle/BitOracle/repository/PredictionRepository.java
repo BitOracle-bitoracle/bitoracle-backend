c103e8b9b5ba3573a0935290cfb56105eddae17c
jhj2075 <flg989@naver.com>
2025-05-13 02:16:00 +0900

    feat: 달력 히스토리 조회 API, 예측 통계 조회 API 구현

--- src/main/java/com/BitOracle/BitOracle/repository/PredictionRepository.java
+++ src/main/java/com/BitOracle/BitOracle/repository/PredictionRepository.java
@@ -1,6 +1,7 @@
 package com.BitOracle.BitOracle.repository;
 
 import com.BitOracle.BitOracle.domain.Prediction;
+import com.BitOracle.BitOracle.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface PredictionRepository extends JpaRepository<Prediction, Long> {
     List<Prediction> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+    List<Prediction> findByUser(User user);
 }
