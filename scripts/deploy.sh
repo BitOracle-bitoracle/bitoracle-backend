9c9b80090b37cf8943026fc2ec869a460ba64450
jhj2075 <flg989@naver.com>
2025-06-14 02:03:04 +0900

    fix: java timezone 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,4 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)    #8
 
 echo "> JAR Name : $JAR_NAME"
 
-sudo nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &       #9
+sudo nohup java -Duser.timezone=Asia/Seoul -jar $REPOSITORY/$JAR_NAME 2>&1 &       #9
